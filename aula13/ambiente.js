4e901b0252685effe393b56ea7baf5d1c3dac0e8
Andrew Müller <126610637+amuller14@users.noreply.github.com>
2023-03-20 16:33:07 +0000

    Aula de Repetições e Debug

---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+// neste codigo o DO executa o bloco console.log antes de WHILE sendo o resultado final equivalente ao de baixo.
+
+
+
+/*
+
+var c = 1
+while (c <= 50) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+neste codigo temos o teste ocorrendo antes da execução do bloco, desta maneira while éexecutado antes do Console.log
+
+*/
+
---
+++ aula14/ambiente.js
@@ -0,0 +1,13 @@
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+console.log('Vai Começar...')
+for (var c = 1;c <= 6;c++) {
+    console.log(`Passo ${c}`)
+}
+console.log('Fim!')
