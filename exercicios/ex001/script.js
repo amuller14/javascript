2f65e95dcd048c09b61bd24cc66f9182f697dc63
Andrew Müller <126610637+amuller14@users.noreply.github.com>
2023-03-29 16:32:37 +0100

    Carregamento emergencial

---
+++ exercicios/ex001/script.js
@@ -0,0 +1,8 @@
+function carregar() {
+
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} Horas`
+}
